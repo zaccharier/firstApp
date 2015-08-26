a89667ef6c1b9ffdedcab2a8084d987ae428a722
zaccharier <zaccharie.ramzi@ekimetrics.com>
2015-08-26 12:19:19 +0000

    added simple validations for weather foreast

--- app/models/weatherforecast.rb
+++ app/models/weatherforecast.rb
@@ -1,2 +1,6 @@
 class Weatherforecast < ActiveRecord::Base
+  validates :office_id, presence: true
+  validates :temp, presence: true
+  validates :date, presence: true
+  validates :sunlight, length: {maximum: 15}
 end
