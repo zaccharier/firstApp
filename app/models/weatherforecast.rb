6a1d6478d94096b0c46f5bfd8d12e6f31adbdba0
zaccharier <zaccharie.ramzi@ekimetrics.com>
2015-08-26 12:40:19 +0000

    added custom validation to weatherforecast to validate existence of office_id

--- app/models/weatherforecast.rb
+++ app/models/weatherforecast.rb
@@ -1,6 +1,13 @@
 class Weatherforecast < ActiveRecord::Base
-  validates :office_id, presence: true
+  validate :validate_office_id
+  validates :office_id, presence: true, numericality: { only_integer: true}, unless: -> { errors.include?(:office_id) }
   validates :temp, presence: true
   validates :date, presence: true
   validates :sunlight, length: {maximum: 15}
+  
+private
+
+  def validate_office_id
+    errors.add(:office_id, "is invalid") unless Office.exists?(self.office_id)
+  end
 end
