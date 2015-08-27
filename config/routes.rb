2df65dddde392924c4121786ce7d001561aed4ee
zaccharier <zaccharie.ramzi@ekimetrics.com>
2015-08-27 15:38:17 +0000

    corrected small mistake

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   resources :weatherforecasts
   resources :offices
-  resources :offices
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -9,8 +8,6 @@ Rails.application.routes.draw do
   #root 'application#welcome'
   root 'static_pages#home'
   get 'info' => 'static_pages#info'
-  
-  resources :offices
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
