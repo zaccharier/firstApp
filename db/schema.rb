3d32fbe8f20321c467e115d5225cf8e5fd10c54e
zaccharier <zaccharie.ramzi@ekimetrics.com>
2015-08-28 08:28:00 +0000

    migrated new table for weather forecasts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826121039) do
+ActiveRecord::Schema.define(version: 20150828082247) do
 
   create_table "offices", force: :cascade do |t|
     t.string   "name"
@@ -22,5 +22,15 @@ ActiveRecord::Schema.define(version: 20150826121039) do
     t.datetime "updated_at",          null: false
   end
 
+  create_table "weatherforecasts", force: :cascade do |t|
+    t.date     "date"
+    t.string   "sunlight"
+    t.integer  "temperature"
+    t.integer  "office_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "weatherforecasts", ["office_id"], name: "index_weatherforecasts_on_office_id"
 
 end
