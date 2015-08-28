2928182f4da946b1799a2362c1b9ea5ad7ecb01b
zaccharier <zaccharie.ramzi@ekimetrics.com>
2015-08-28 07:57:33 +0000

    made a ~good looking~ show page for the offices.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :weatherforecasts
+  #resources :weatherforecasts
   resources :offices
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
